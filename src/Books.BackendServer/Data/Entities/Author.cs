8e64679bac5cd5cbe1eae7c2864afb355febe82d
nnmchau01 <nguyenchau0926@gmail.com>
2023-04-11 03:08:37 +0700

    Tạo class

---
+++ src/Books.BackendServer/Data/Entities/Author.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Books.BackendServer.Data.Entities
+{
+    [Table("Authors")]
+    public class Author
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        [Column(TypeName = "nvarchar(255)" )]
+        [Required]
+        public string ?Name { get; set; }
+
+        [Required]
+        public bool? Female { get; set; }
+
+        [Required]
+        public int Born { get; set; }
+
+        public int Died { get; set; }
+
+    }
+}
---
+++ src/Books.BackendServer/Data/Entities/Book.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Books.BackendServer.Data.Entities
+{
+    [Table("Books")]
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        [Column(TypeName = "nvarchar(255)")]
+        [Required]
+
+        public string? Title { get; set; }
+
+        [MaxLength(255)]
+        [Column(TypeName = "nvarchar(255)")]
+        public string? Topic { get; set; }
+
+
+        [Required]
+        public int AuthorId { get; set; }
+        public int PublishYear { get; set; }
+
+        [MaxLength(50)]
+        [Column(TypeName = "tinyint")]
+        public decimal Price { get; set; }
+        public int Rating { get; set; }
+        
+        
+    }
+}
